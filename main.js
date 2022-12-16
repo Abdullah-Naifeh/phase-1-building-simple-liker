f84e6d20056f33f385b643aef50948d2fd858f03
Abdullah-Naifeh <abdna59@gmail.com>
2022-12-16 18:06:07 +0300

    Assignment Completed

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
+const errorModal = document.getElementById('modal')
+errorModal.classList.add("hidden")
+const errorMessage = document.getElementById('modal-message')
+const heartSpan = document.querySelector('.like-glyph')
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+heartSpan.addEventListener('click', () =>{
+  mimicServerCall()
+  .then(() => {
+    if(heartSpan.innerHTML === FULL_HEART){
+      heartSpan.innerHTML = EMPTY_HEART
+      heartSpan.classList.remove('activated-heart')
+    } else {    
+    heartSpan.innerHTML = FULL_HEART
+    heartSpan.classList.add('activated-heart')}
+  })
+  .catch(error => {
+    errorModal.classList.remove('hidden')
+    errorMessage.innerHTML = error
+    setTimeout(() =>{errorModal.classList.add('hidden')},3000)
+  })
+});
 
 
 
